717ddb619230ce32dcabf8233ff4d7cf2076f422
Reyes Sanchez Daniel (RBMX/BUD1-IO2) <Daniel.ReyesSanchez@mx.bosch.com>
2019-05-05 18:15:55 -0500

    updated android project in order to use shared library. Also created iOS project then embedded shared library inside

---
+++ iosApp/GitHubKMP/GitHubKMP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubKMP
+//
+//  Created by Daniel Reyes Sánchez on 5/5/19.
+//  Copyright © 2019 Robert Bosch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
