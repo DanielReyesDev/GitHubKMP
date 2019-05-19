7c2a0d942a03846325708a8b8eabbaed6b60a783
Reyes Sanchez Daniel (RBMX/BUD1-IO2) <Daniel.ReyesSanchez@mx.bosch.com>
2019-05-19 08:26:44 -0500

    manually generated the xcode-frameworks again. It was erased due to a clean-up. Also preparing iOS Project to act as a presenter

---
+++ iosApp/GitHubKMP/GitHubKMP/BaseView.swift
@@ -0,0 +1,11 @@
+//
+//  BaseView.swift
+//  GitHubKMP
+//
+//  Created by Daniel Reyes Sánchez on 5/19/19.
+//  Copyright © 2019 Robert Bosch. All rights reserved.
+//
+
+import Foundation
+import shared
+import UIKit
